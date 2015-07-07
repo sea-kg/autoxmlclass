a625f2bdb07b3df77e951c94bc1055847532651b
Evgenii Sopov <eso@startext.tomsk.ru>
2015-07-07 15:17:48 +0600

    added config file

---
+++ www/config.php
@@ -0,0 +1,30 @@
+<?php
+	$config = Array();
+	
+	$config['langs'] = Array();
+	
+	$config['langs']['cpp_qt_useqxml'] = Array();
+	$config['langs']['cpp_qt_useqxml']['name'] = 'C++ & Qt 5.1 (QXmlReader, QXmlWriter)';
+	$config['langs']['cpp_qt_useqxml'][''] = 
+	$config['langs']['cpp_qt_useqxml']['include_file'] = 'generators/cpp_qt_useqxml.php';
+		
+	$config['langs']['cpp_cbuilderxe3_usexml'] = Array();
+	$config['langs']['cpp_cbuilderxe3_usexml']['name'] = 'C++ Builder XE3 & TXMLDocument';
+	$config['langs']['cpp_cbuilderxe3_usexml']['include_file'] = 'generators/cpp_cbuilderxe3_usexml.php';
+	
+	$config['langs']['java_usexml'] = Array();
+	$config['langs']['java_usexml']['name'] = 'Java (todo)';
+	$config['langs']['java_usexml']['include_file'] = 'generators/java_usexml.php';
+	
+	$config['langs']['python_usexml'] = Array();
+	$config['langs']['python_usexml']['name'] = 'Python (todo)';
+	$config['langs']['python_usexml']['include_file'] = 'generators/python_usexml.php';
+	
+	$config['langs']['php_usexml'] = Array();
+	$config['langs']['php_usexml']['name'] = 'PHP 5 (todo)';
+	$config['langs']['php_usexml']['include_file'] = 'generators/php_usexml.php';
+	
+	$config['langs']['csharp_usexml'] = Array();
+	$config['langs']['csharp_usexml']['name'] = 'C# (todo)';
+	$config['langs']['csharp_usexml']['include_file'] = 'generators/csharp_usexml.php';
+	
